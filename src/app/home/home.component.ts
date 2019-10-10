963c95f7750e41d5f7c596595b6cc679eed5f41a
rookmoose <rookmoose@gmail.com>
2019-10-10 16:18:03 -0700

    test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,6 +11,9 @@ export class HomeComponent {
   otherNumbers: number[] = [];
 
   constructor() { 
+
+    //I'm testing this shizz
+
     for(let i = 0; i < 10; i++){
       this.numbers.push(i);
     }
