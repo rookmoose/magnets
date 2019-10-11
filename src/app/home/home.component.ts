1fbda75889206e7fe8a179bb4229e2f58cbfffce
rookmoose <rookmoose@gmail.com>
2019-10-11 16:10:56 -0700

    Logic set to check a solution

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,13 +7,33 @@ import { CdkDragDrop, transferArrayItem, moveItemInArray } from '@angular/cdk/dr
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  numbers: number[] = [];
-  otherNumbers: number[] = [];
+  original: number[] = [];
+  expected: number[] = [];
+  solution: number[] = [];
 
   constructor() { 
-    for(let i = 0; i < 10; i++){
-      this.numbers.push(i);
+    for(var i = 0; i < 5; i++){
+      this.original.push(i);
+      this.expected.push(i);
     }
+    for(var i = this.original.length - 1; i > 0; i--){
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = this.original[i];
+      this.original[i] = this.original[j];
+      this.original[j] = temp;
+    }
+  }
+
+  checkSolution(){
+    for(var i = 0; i < this.expected.length; i++){
+      if (this.solution[i] != this.expected[i])
+        return false;
+    }
+    return true;
+  }
+
+  getSecondListPopulated() {
+    return this.expected.length == this.solution.length;
   }
 
   drop(event: CdkDragDrop<number[]>) {
